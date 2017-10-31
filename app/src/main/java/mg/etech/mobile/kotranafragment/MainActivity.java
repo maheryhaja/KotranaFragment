02f097a4ebd4873fa11bd472e6336e32ffd127ce
maheryhaja <ygunion@gmail.com>
2017-10-31 17:06:28 +0300

    Initial commit

---
+++ app/src/main/java/mg/etech/mobile/kotranafragment/MainActivity.java
@@ -0,0 +1,13 @@
+package mg.etech.mobile.kotranafragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
